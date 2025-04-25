fe1c18c5b2d4fb89e94f9878bb660deb32ac794b
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-04-25 19:31:48 +0100

    slide-herosection

--- assets/script/input.js
+++ assets/script/input.js
@@ -1,35 +1,34 @@
-/* var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    grabCursor: true,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  }); */ 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-  document.querySelectorAll('.featured-image-1').forEach((image) => {
-    image.addEventListener('click', () => {
-      const imageSrc = image.getAttribute('src')
-      document.querySelector('.big-image-1').setAttribute('src', imageSrc)
-    })
-  })
+document.querySelectorAll(".featured-image-1").forEach((image) => {
+  image.addEventListener("click", () => {
+    const imageSrc = image.getAttribute("src");
+    document.querySelector(".big-image-1").setAttribute("src", imageSrc);
+  });
+});
 
-  document.querySelectorAll('.featured-image-2').forEach((image) => {
-    image.addEventListener('click', () => {
-      const imageSrc = image.getAttribute('src')
-      document.querySelector('.big-image-2').setAttribute('src', imageSrc)
-    })
-  })
+document.querySelectorAll(".featured-image-2").forEach((image) => {
+  image.addEventListener("click", () => {
+    const imageSrc = image.getAttribute("src");
+    document.querySelector(".big-image-2").setAttribute("src", imageSrc);
+  });
+});
 
-  document.querySelectorAll('.featured-image-3').forEach((image) => {
-    image.addEventListener('click', () => {
-      const imageSrc = image.getAttribute('src')
-      document.querySelector('.big-image-3').setAttribute('src', imageSrc)
-    })
-  })
-
+document.querySelectorAll(".featured-image-3").forEach((image) => {
+  image.addEventListener("click", () => {
+    const imageSrc = image.getAttribute("src");
+    document.querySelector(".big-image-3").setAttribute("src", imageSrc);
+  });
+});
