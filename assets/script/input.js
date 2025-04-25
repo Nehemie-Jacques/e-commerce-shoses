4ae54fcc87e335443047e8d34c622b0ba609b7b9
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-04-25 08:40:25 +0100

    featured-products-change

--- assets/script/input.js
+++ assets/script/input.js
@@ -12,4 +12,24 @@
     },
   }); */ 
 
-  
+  document.querySelectorAll('.featured-image-1').forEach((image) => {
+    image.addEventListener('click', () => {
+      const imageSrc = image.getAttribute('src')
+      document.querySelector('.big-image-1').setAttribute('src', imageSrc)
+    })
+  })
+
+  document.querySelectorAll('.featured-image-2').forEach((image) => {
+    image.addEventListener('click', () => {
+      const imageSrc = image.getAttribute('src')
+      document.querySelector('.big-image-2').setAttribute('src', imageSrc)
+    })
+  })
+
+  document.querySelectorAll('.featured-image-3').forEach((image) => {
+    image.addEventListener('click', () => {
+      const imageSrc = image.getAttribute('src')
+      document.querySelector('.big-image-3').setAttribute('src', imageSrc)
+    })
+  })
+
